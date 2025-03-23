02ab3f62f6f21669b0a1ffa65c3c36389a1b2f4d
pravinagale500 <pravinagale500@gmail.com>
2025-03-23 16:00:35 +0530

    Commented Make My Trip Test Case

--- tests/MakeMyTrip.spec.js
+++ tests/MakeMyTrip.spec.js
@@ -1,73 +1,73 @@
-const {test,expect} = require('@playwright/test')
+// const {test,expect} = require('@playwright/test')
 
 
-test('MakeMyTrip Flights', async ({page})=>{
+// test('MakeMyTrip Flights', async ({page})=>{
 
-    await page.goto("https://www.makemytrip.com/", { timeout: 60000 });
+//     await page.goto("https://www.makemytrip.com/", { timeout: 60000 });
 
-   // Expect a title "to contain" a substring.
-    const pageTitle = await expect(page).toHaveTitle(/MakeMyTrip - #1 Travel Website 50% OFF on Hotels, Flights & Holiday/);
-    console.log(pageTitle);
+//    // Expect a title "to contain" a substring.
+//     const pageTitle = await expect(page).toHaveTitle(/MakeMyTrip - #1 Travel Website 50% OFF on Hotels, Flights & Holiday/);
+//     console.log(pageTitle);
 
-    const fromCityName = "Mumbai, India";
-    const toCityName = "Bengaluru, India";
-    const dayPickerDay = "Mon Mar 24 2025";
-    const dayPickerReturnDay = "Thu Mar 27 2025";
-    const closeModal =page.locator("span.commonModal__close");
-    const fromCityLocator = page.locator("#fromCity");
-    const fromInputLocator= page.locator("input[placeholder='From']");
-    const fromCities = page.locator("//p[text()='"+fromCityName+"']");
-    const toCityLocator = page.locator("#toCity");
-    const toInputLocator= page.locator("input[placeholder='To']");
-    const toCities = page.locator("//p[text()='"+toCityName+"']");
-    const dayPicker = page.locator("(//div[@class='DayPicker'])[1]");
-    const dayPickerDayLocator = page.locator("div[aria-label='"+dayPickerDay+"'] p:nth-child(1)");
-    const tapOnReturnDate = page.locator("p.latoBlack.font12.greyText.lineHeight16")
-    const dayPickerDayReturnLocator = page.locator("div[aria-label='"+dayPickerReturnDay+"'] p:nth-child(1)");
+//     const fromCityName = "Mumbai, India";
+//     const toCityName = "Bengaluru, India";
+//     const dayPickerDay = "Mon Mar 24 2025";
+//     const dayPickerReturnDay = "Thu Mar 27 2025";
+//     const closeModal =page.locator("span.commonModal__close");
+//     const fromCityLocator = page.locator("#fromCity");
+//     const fromInputLocator= page.locator("input[placeholder='From']");
+//     const fromCities = page.locator("//p[text()='"+fromCityName+"']");
+//     const toCityLocator = page.locator("#toCity");
+//     const toInputLocator= page.locator("input[placeholder='To']");
+//     const toCities = page.locator("//p[text()='"+toCityName+"']");
+//     const dayPicker = page.locator("(//div[@class='DayPicker'])[1]");
+//     const dayPickerDayLocator = page.locator("div[aria-label='"+dayPickerDay+"'] p:nth-child(1)");
+//     const tapOnReturnDate = page.locator("p.latoBlack.font12.greyText.lineHeight16")
+//     const dayPickerDayReturnLocator = page.locator("div[aria-label='"+dayPickerReturnDay+"'] p:nth-child(1)");
 
-    const travellersLocator = page.locator("span[class='appendRight10']");
-    const adultsLocator =page.locator("li[data-cy='adults-2']")
-    const applyBtnLocator = page.getByRole('button', {name:'APPLY'});
-    const searchBtnLocator = page.locator("a.primaryBtn.font24.latoBold.widgetSearchBtn");
+//     const travellersLocator = page.locator("span[class='appendRight10']");
+//     const adultsLocator =page.locator("li[data-cy='adults-2']")
+//     const applyBtnLocator = page.getByRole('button', {name:'APPLY'});
+//     const searchBtnLocator = page.locator("a.primaryBtn.font24.latoBold.widgetSearchBtn");
 
-    const today = new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
+//     const today = new Date().toISOString().split('T')[0]; // Format: YYYY-MM-DD
    
-    // await page.fill("//div[@class='DayPicker-Day DayPicker-Day--selected DayPicker-Day--today']", today);
-    // const futureDate = new Date();
-    // futureDate.setDate(futureDate.getDate() + 7); // Add 7 days
-    //  const formattedDate = futureDate.toISOString().split('T')[0];
-    //    await page.fill("//div[@class='DayPicker-Day DayPicker-Day--selected DayPicker-Day--today']", formattedDate);
+//     // await page.fill("//div[@class='DayPicker-Day DayPicker-Day--selected DayPicker-Day--today']", today);
+//     // const futureDate = new Date();
+//     // futureDate.setDate(futureDate.getDate() + 7); // Add 7 days
+//     //  const formattedDate = futureDate.toISOString().split('T')[0];
+//     //    await page.fill("//div[@class='DayPicker-Day DayPicker-Day--selected DayPicker-Day--today']", formattedDate);
 
-    //close modal
-    await closeModal.click();
-    //Select from city
-    await fromCityLocator.click();
-    await fromInputLocator.fill(fromCityName);
-    await fromCities.click();
-    //Select to city
-    await toCityLocator.click();
-    await toInputLocator.fill(toCityName);
-    await toCities.click();
+//     //close modal
+//     await closeModal.click();
+//     //Select from city
+//     await fromCityLocator.click();
+//     await fromInputLocator.fill(fromCityName);
+//     await fromCities.click();
+//     //Select to city
+//     await toCityLocator.click();
+//     await toInputLocator.fill(toCityName);
+//     await toCities.click();
 
-    await dayPicker.isVisible();
-    //SelectFromDate
-    await dayPickerDayLocator.click();
+//     await dayPicker.isVisible();
+//     //SelectFromDate
+//     await dayPickerDayLocator.click();
     
-    //SelectReturnDate
-    await tapOnReturnDate.click();
-    await dayPickerDayReturnLocator.click();
-    //Select travellers
-    await travellersLocator.click();
-    await adultsLocator.click();
-    await applyBtnLocator.click();
+//     //SelectReturnDate
+//     await tapOnReturnDate.click();
+//     await dayPickerDayReturnLocator.click();
+//     //Select travellers
+//     await travellersLocator.click();
+//     await adultsLocator.click();
+//     await applyBtnLocator.click();
 
-    //click on Search button
-    // await page.waitForSelector(searchBtnLocator);
-    await searchBtnLocator.click();
+//     //click on Search button
+//     // await page.waitForSelector(searchBtnLocator);
+//     await searchBtnLocator.click();
 
-   //close the page
-    page.close();
+//    //close the page
+//     page.close();
 
 
     
-})
+// })
