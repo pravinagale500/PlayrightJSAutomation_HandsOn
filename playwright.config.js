123d25ba90c1b99b856808bbb05bfaff27e868f4
pravinagale500 <pravinagale500@gmail.com>
2025-03-28 13:02:59 +0530

    Changed Timeout in playwright config js

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  timeout:60*1000,
+  timeout:50*1000,
   expect:{
     timeout: 30*1000,
   },
